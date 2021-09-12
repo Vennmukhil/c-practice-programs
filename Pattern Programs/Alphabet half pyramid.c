417e934690542d580c66c7db302a5dde356c6a96
Vennmukhil <65287939+Vennmukhil@users.noreply.github.com>
2021-09-12 21:34:52 +0530

    C Pattern Programs

---
+++ Pattern Programs/Alphabet half pyramid.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+  char i,j,lastalph;
+  printf("Enter the last alph");
+  scanf("%c",&lastalph);
+  for(i='A';i<=lastalph;i++){
+    for(j='A';j<=i;j++){
+      printf("%c ",i);  // i for same alph in each row & j for incremented alph
+    }
+    printf("\n");
+  }
+  return 0;
+}
