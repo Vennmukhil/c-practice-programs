b927189e8f6ac360dd2ee3c5f30212ef5c890897
Vennmukhil <65287939+Vennmukhil@users.noreply.github.com>
2021-09-12 21:40:08 +0530

    Add files via upload

---
+++ Pattern Programs/starfullpyramid.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+  int i,j,k,rows,sp;
+  printf("Enter the no of rows:");
+  scanf("%d",&rows);
+  for(i=1;i<=rows;i++){
+    for(sp=rows-i;sp>=1;sp--){
+      printf("  ");
+    }
+    for(k=1;k<=(i*2)-1;k++){
+      printf("* ");
+    }
+    printf("\n");
+  }
+  return 0;
+}
